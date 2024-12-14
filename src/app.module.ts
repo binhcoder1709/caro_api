11f1953eff5ae1d5de2b93a114b4d6972de26ec1
binhcoder1709 <hn6006077@gmail.com>
2024-12-14 16:57:03 +0700

    update configure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,16 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {AppGateway} from "./app.gateway";
 import Redis from "ioredis";
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [],
+    imports: [ConfigModule.forRoot({envFilePath: '.env', isGlobal: true})],
     controllers: [AppController],
     providers: [AppService, AppGateway, {
         provide: 'REDIS_CLIENT',
         useFactory: () => {
             return new Redis({
-                host: 'localhost',
+                host: process.env.REDIS_HOST,
                 port: 6379
             })
         }
